7362857cde699bf9b77322528b4cdec92698bfae
Shivambembey98 <shivam.bembey@bellblazetech.com>
2025-10-08 02:37:42 +0530

    Create version.tf

---
+++ version.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "shivam-terraform-state-bucket"
+    key    = "demo/ec2/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
